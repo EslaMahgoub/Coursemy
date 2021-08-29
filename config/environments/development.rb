5aec666030f3a27d973b322997904898c6aa04aa
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2021-08-29 15:54:20 +0000

    Apply send grid for sending emails in production

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   config.hosts << "21989e445acd43a39688b05a9160877d.vfs.cloud9.us-east-1.amazonaws.com"
   config.action_mailer.default_url_options = { host: 'https://21989e445acd43a39688b05a9160877d.vfs.cloud9.us-east-1.amazonaws.com/'}
+  config.action_mailer.delivery_method = :test
   # Settings specified here will take precedence over those in config/application.rb.
 
   # In the development environment your application's code is reloaded any time
