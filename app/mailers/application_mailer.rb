d126d055abebe5d86c2d44547553c5bfa6d45eac
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2021-08-30 09:47:15 +0000

    Modify default email sender in production

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'eslamkhaledtawfik@gmail.com'
+  #default from: 'eslamkhaledtawfik@gmail.com'
+  default from: 'Coursemy <eslamkhaledtawfik@gmail.com>'
   layout 'mailer'
 end
