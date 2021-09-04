5e04c3ddad8e87bcdee81b71cf80bac24dd11da5
Eslam Mahgoub <eslamkhaledtawfik@gmail.com>
2021-09-04 13:56:11 +0000

    Fix Activity Error with lessons Modify Activity View

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,29 @@
 module ApplicationHelper
   include Pagy::Frontend
+  
+  
+  # create dynamic labels based on crud action
+  def crud_label(key)
+    case key
+      when 'create'
+        "<i class='fas fa-plus'></i>".html_safe
+      when 'update'
+        "<i class='fas fa-pen'></i>".html_safe
+      when 'destroy'
+        "<i class='fas fa-trash'></i>".html_safe
+    end
+  end
+  
+  # create dynamic labels based on model
+  def model_label(model)
+    case model
+      when 'Course'
+        "<i class='fas fa-graduation-cap'></i>".html_safe
+      when 'Lesson'
+        "<i class='fas fa-tasks'></i>".html_safe
+    end
+  end
+  
+  
+  
 end
